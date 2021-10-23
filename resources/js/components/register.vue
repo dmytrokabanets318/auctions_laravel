dee122f795beb92d1a4468f6c2ebbcfd3b0bdd90
dmytrokabanets8 <dmytrokabanets8@gmail.com>
2021-10-23 14:31:17 -0500

    Update register.vue

--- resources/js/components/register.vue
+++ resources/js/components/register.vue
@@ -5,106 +5,120 @@
 			<div class="col"></div>
 			<div class="col-6">
 				<div class="form-group align-midle" style="margin-top: 100px;">
-
-					<alertBox :message="alert.message" :type="alert.type" :show="alert.show"></alertBox>
+          <h2 class="h2 text-center">Register</h2>
 
 					<form class="form-group">
-						<h2 class="h2 text-center">Register</h2>
 						<label for="name">Name</label>
-						<input class="form-control" type="text" v-model="user.name" required></input>
+						<input class="form-control" type="text" v-model="user.name" required>
 						<label for="Email">Email</label>
 						<input class="form-control" type="email" name="email" v-model="user.email" required>
 						<label for="password">Password</label>
 						<input class="form-control" type="password" name="password" v-model="user.password" required>
 						<label for="passwordConfirmation">Confirm Password</label>
 						<input class="form-control" type="password" v-model="confPass" required>
-						<br>	
-						<button class="btn btn-success form-control" type="button" @click.prevent="registerUser">Register</button>
+						<button class="btn btn-success form-control mt-4" type="button" @click.prevent="registerUser">Register</button>
 					</form>
+
+					<alertBox @close="alert.show = false" :errors="errors" :message="alert.message" :type="alert.type" :show="alert.show"></alertBox>
+
 				</div>
 			</div>
 			<div class="col"></div>
 		</div>
 	</div>
-</div>	
 
 </template>
 
 <script type="text/javascript">
-	
-	import alertBox from './alertBox.vue';
+import alertBox from "./alertBox.vue";
 
-	export default {
+export default {
+  components: {
+    alertBox,
+  },
 
-		components: {
+  data() {
+    return {
+      user: {
+        name: "",
+        password: "",
+        email: "",
+      },
 
-			alertBox,
+      confPass: "",
+      alert: {
+        type: "alert alert-danger",
+        show: false,
+        message: "",
+      },
 
-		},
+      errors: [],
 
-		data() {
+    };
+  },
 
-			return {
+  methods: {
+		
+    registerUser() {
 
-				user: {
+      if(!this.validateForm()){
+        return;
+      }
 
-					name: "",
-					password: "",
-					email: "",
+      this.$toasted.show("Creating your account...");
+      this.$axios.post("api/register", this.user).then((response) => {
+        if (response.data.code != 200) {
+          this.alert.message = response.data.message;
+          this.alert.show = true;
+        } else {
+          Vue.toasted.success(response.data.message);
+          this.$router.push("/login");
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+    },
+    
+    validateForm(){
 
-				},
+      this.alert.show = false;
+      this.errors = [];
 
-				confPass: "",
-				alert: {
+      const user = this.user;
+      let newErrors = [];
 
-					type: "alert alert-danger",
-					show: false,
-					message: "",
+      if(!user.name){
+        newErrors.push('Name is required');
+      }
 
-				},
+      if(!user.email){
+        newErrors.push('Email is required');
+      }else if(!this.validEmail(user.email)){
+        newErrors.push('Email must be a valid email');
+      }
 
-			}
-		},
+      if(!user.password){
+        newErrors.push('Password is required');
+      }else if(user.password != this.confPass){
+        newErrors.push('Passwords must match');
+      }
 
-		methods: {
+      if(newErrors.length){
+        this.errors.push(...newErrors);
+        this.alert.show = true;
+        return false;
+      }
 
-			registerUser(){
+      return true;
 
-				console.log(this.user);
+    },
 
-				this.alert.show = false;
+    validEmail(email){
+      const regex = /^((?!\.)[\w\-_.]*[^.])(@\w+)(\.\w+(\.\w+)?[^.\W])$/;
+      return regex.test(email);
+    }
 
-				if(this.confPass != this.user.password){
-
-					this.alert.message = "Passwords must match!";
-					this.alert.type = "alert alert-danger";
-					this.alert.show = true;
-
-				}else{
-					this.$toasted.show('Creating your account...');
-					this.$axios.post('api/register', this.user)
-					.then(response => {
-
-						if(response.data.code != 200){
-							this.alert.message = response.data.message;
-							this.alert.show = true;
-						}else{
-							Vue.toasted.show(response.data.message);
-							this.$router.push('/login');
-						}
-
-					})
-					.catch(error => {
-						this.alert.show = true;
-						this.alert.type = "alert alert-danger";
-					})
-				}
-
-
-			},
-
-		}
-
-	}
-
+  }
+}
 </script>
