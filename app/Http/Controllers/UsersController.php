e4db7ed9ec40f1778b7056f3a8b51f215dec2cbe
Rui Inácio <rmy_98@hotmail.com>
2021-11-03 13:25:59 -0500

    Refactored major parts of the app
    Login, register, myAuctions and Controlller Responses

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -43,9 +43,9 @@ class UsersController extends Controller{
         $credentials = $request->only('email', 'password');
 
         if(Auth::attempt($credentials, $request->remember)){
-            return ["user" => Auth::user(), "wallet" => Auth::user()->wallet];
+            return response()->json(["user" => Auth::user(), "wallet" => Auth::user()->wallet, 'api_token' => Auth::user()->remember_token]);
         }else{
-            return ["message" => "Invalid credentials", "code" => 401];
+            return response()->json(["message" => "Invalid credentials"], 406);
         }
 
     }
--- resources/js/components/login.vue
+++ resources/js/components/login.vue
@@ -2,22 +2,35 @@
   <div class="container">
     <div class="row">
       <div class="col"></div>
-      <div class="col-5" style="margin-top: 150px;">
-
+      <div class="col-5" style="margin-top: 150px">
         <form action="#" class="form-group">
           <h1 class="display-3 text-center">Login</h1>
           <label for="email">Email</label>
-          <input type="email" required v-model="user.email" class="form-control">
+          <input
+            type="email"
+            required
+            v-model="user.email"
+            class="form-control"
+          />
           <label for="password">Password</label>
-          <input type="password" required v-model="user.password" class="form-control">
-          <br>
-          <button class="btn btn-success form-control" @click.prevent="login">Login</button>
-          <br>
-          <AlertBox class="mt-3"
-            :message="alert.message" 
-            :type="alert.type" 
+          <input
+            type="password"
+            required
+            v-model="user.password"
+            class="form-control"
+          />
+          <br />
+          <button class="btn btn-success form-control" @click.prevent="login">
+            Login
+          </button>
+          <br />
+          <AlertBox
+            class="mt-3"
+            :message="alert.message"
+            :type="alert.type"
             :show="alert.show"
-            @close="alert.show = false">
+            @close="alert.show = false"
+          >
           </AlertBox>
           <p class="text-center mt-4">
             <router-link to="forgotPassword">
@@ -25,7 +38,6 @@
             </router-link>
           </p>
         </form>
-
       </div>
       <div class="col"></div>
     </div>
@@ -34,78 +46,72 @@
 
 <script>
 
-  export default {
+import { mapMutations } from 'vuex';
 
-    components: {
-      AlertBox: () => import('./alertBox'),
-    },
+export default {
 
-    data(){
-      return {
+  components: {
+    AlertBox: () => import("./utils/alertBox.vue"),
+  },
 
-        user: {
-          email : "",
-          password: "",
-        },
-        alert: {
-          message : "",
-          type: "alert alert-danger",
-          show: false,
-        }
+  data() {
+    return {
+      user: {
+        email: "",
+        password: "",
+      },
+      alert: {
+        message: "",
+        type: "alert alert-danger",
+        show: false,
+      },
+    };
+  },
 
+  methods: {
+    login() {
+
+      this.alert.show = false;
+
+      if (this.user.name == "") {
+        this.alert.message = "Please insert your credentials";
+        this.alert.show = true;
+        return;
       }
-    },
-
-    methods: {
-
-      login(){
-
-        this.alert.show = false;
-
-        if(this.user.name == ""){
-
-          this.alert.message = "Please insert your credentials";
-          this.alert.show = true;
-
-        }else if(this.user.password == ""){
-
-          this.alert.show = true;
-          this.alert.message = "Please insert your credentials";
-
-        }else {
-
-          Vue.toasted.show("Loggin in...");
-          this.alert.show = false;
-          this.$axios.post('api/login', this.user)
-          .then(response => {
-
-            if(response.data.code == 401){
-              this.alert.message = response.data.message;
-              this.alert.show = true;
-            }else{
-              Vue.toasted.success("Logged in!");
-              this.$store.commit('storeUserToken', response.data.token);
-              this.$store.commit('storeUser', response.data.user);
-              this.$store.commit('storeUserId', response.data.id);
-              this.$router.push('/auctions');
-            }
-
-
-          })
-          .catch(error => {
-
-            this.alert.show = true;
-            this.alert.message = "Error: " + error.response;
-            console.log(error);
-
-          });
-
-        }
-
+      
+      if (this.user.password == "") {
+        this.alert.message = "Please insert your credentials";
+        this.alert.show = true;
+        return;
       }
 
-    }
+      Vue.toasted.show("Loggin in...");
+      this.alert.show = false;
+      this.$axios
+        .post("api/login", this.user)
+        .then((response) => {
 
-  }
+          Vue.toasted.success("Logged in!");
 
+          const user = response.data.user;
+
+          this.$store.commit("storeUserToken", response.data.api_token);
+          this.$store.commit("storeUser", user);
+          this.$store.commit("storeUserId", user.id);
+          this.$store.commit('storeWallet', user.wallet);
+
+          this.$router.push("/auctions");
+          
+        })
+        .catch((error) => {
+          this.alert.show = true;
+          if (error.response.status == 406) {
+            this.alert.message = "Error: " + error.response.data.message;
+          }
+          console.log(error);
+        });
+      
+    },
+  },
+};
 </script>
