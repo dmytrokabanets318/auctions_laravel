e89a66f7eb5f3bc32a3b094a7868994f5590265e
dmytrokabanets8 <dmytrokabanets8@gmail.com>
2022-10-14 15:50:41 +0100

    Configs

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+if (App::environment('production')) {
+    URL::forceScheme('https');
+}
+
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
